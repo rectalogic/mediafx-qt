403337a60d69f798d41b7a157eea6b95ad432173
Andrew Wason <rectalogic@rectalogic.com>
2023-10-21 10:44:43 -0400

    GPLv3

--- builders/linux/docker-run.sh
+++ builders/linux/docker-run.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright (C) 2023 Andrew Wason
+#
+# This file is part of mediaFX.
+#
+# mediaFX is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# mediaFX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with mediaFX.
+# If not, see <https://www.gnu.org/licenses/>.
 
 MOUNT="$(dirname $(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"))"
 
--- builders/linux/mediafx-build.sh
+++ builders/linux/mediafx-build.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright (C) 2023 Andrew Wason
+#
+# This file is part of mediaFX.
+#
+# mediaFX is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# mediaFX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with mediaFX.
+# If not, see <https://www.gnu.org/licenses/>.
 
 cd /webvfx/build/linux
 cmake --install-prefix /usr/local/Qt/${QT_VER}/gcc_64 ../.. && cmake --build . && sudo cmake --install .
