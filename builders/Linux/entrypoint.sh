9f234a27b369339be16956253aff6fdaf9c71a22
Andrew Wason <rectalogic@rectalogic.com>
2024-01-03 17:48:01 -0500

    fix docker bind mount permission issues

--- builders/Linux/entrypoint.sh
+++ builders/Linux/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /mediafx/build/Linux && sudo chown mediafx /mediafx/build/ /mediafx/build/Linux
-exec "$@"
+OLD_UID=$(id -u mediafx)
+NEW_UID=$(stat -c "%u" /mediafx/CMakeLists.txt)
+if [ ! -z "$NEW_UID" -a "$NEW_UID" != "0" -a "$OLD_UID" != "$NEW_UID" ]; then
+    usermod -u "$NEW_UID" -o mediafx
+fi
+
+mkdir -p /mediafx/build/Linux && chown -R mediafx /mediafx/build/Linux
+exec gosu mediafx /entrypoint/xvfb.sh "$@"
