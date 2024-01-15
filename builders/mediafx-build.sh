08b7936d057ae3d581b8763c10f86c3001fb4705
Andrew Wason <rectalogic@rectalogic.com>
2024-01-15 11:52:05 -0500

    make sudo conditional

--- builders/mediafx-build.sh
+++ builders/mediafx-build.sh
@@ -9,7 +9,7 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 MEDIAFX_BUILD="${BUILD_ROOT}/${BUILD_TYPE}"
 mkdir -p "$MEDIAFX_BUILD"
 cd "$MEDIAFX_BUILD"
-(cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE --install-prefix "${QTDIR}" ../../.. && cmake --build . && sudo cmake --install .) || exit 1
+(cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE --install-prefix "${QTDIR}" ../../.. && cmake --build . && ${SUDO} cmake --install .) || exit 1
 if [[ -v MEDIAFX_TEST ]]; then
     make test CTEST_OUTPUT_ON_FAILURE=1 ARGS="${MEDIAFX_TEST}" || exit 1
 fi
